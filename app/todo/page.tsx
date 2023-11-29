61fd7a8c571f6d48cbdc8b7396aad7320777d56e
Maksim <MaksimAndreevich@users.noreply.github.com>
2023-11-29 21:17:02 +0300

    todolist draft

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -4,7 +4,12 @@ import Todo from "../ui/Todo";
 
 const todosFromServer: Array<ITodo> = [
   { id: "adf", title: "afasf", status: "done" },
-  { id: "asfasf", title: "afaasfasfsf", status: "done" },
+  {
+    id: "asfasf",
+    title:
+      "afaasfasfsf afaasfasfsf afafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsffsf afaasfasfsf afaasfasfsfafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsfaasfasfsf afaasfasafaasfasfsf afaasfasfsf afaasfasfsf afaasfasfsffsf",
+    status: "done",
+  },
   { id: "232s", title: "afasaf asfasfsaf  s afasfasf sasf", status: "pending" },
   {
     id: "asfa",
--- app/ui/TodoList/index.tsx
+++ app/ui/TodoList/index.tsx
@@ -1,10 +1,49 @@
-import { Box } from "@mui/material";
+import {
+  Checkbox,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { observer } from "mobx-react-lite";
 import { ITodo } from "../../lib/interfaces";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const TodoList = observer(
   ({ todosFromServer }: { todosFromServer: Array<ITodo> }) => {
-    return <Box></Box>;
+    return (
+      <List>
+        {todosFromServer.map(({ id, title, status }) => (
+          <ListItem
+            key={id}
+            secondaryAction={
+              <IconButton edge="end" aria-label="delete todo">
+                <DeleteIcon />
+              </IconButton>
+            }
+            disablePadding
+          >
+            <ListItemButton
+              //   onClick={handleDone(value)}
+              dense
+            >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={status === "done"}
+                  tabIndex={-1}
+                  disableRipple
+                  //   inputProps={{ "aria-labelledby": labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={id} primary={title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    );
   }
 );
 
