e328bcbbfd72c44775da5e46492cac17e085836d
Maksim <maksim.e@mail.ru>
2024-01-17 18:00:17 +0300

    test priority images

--- ui/FlipCard/index.tsx
+++ ui/FlipCard/index.tsx
@@ -63,6 +63,7 @@ const FlipCard = observer(({ sx, card }: FlipCardProps) => {
 
           <Box className="flip-card-back">
             <Image
+              priority
               src={image.urls.regular}
               width={350}
               height={250}
