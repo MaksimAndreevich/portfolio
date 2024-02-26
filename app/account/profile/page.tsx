e921f14adcf5ade59a0100c69b3752f90060f49e
makcext <makcbowka@gmail.com>
2024-02-26 15:08:44 +0200

    then session is exist, map user data

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -3,12 +3,21 @@ import { auth } from "../../../auth";
 import Profile from "../../../ui/Profile";
 import routes from "../../../lib/routes";
 
+import { IUser } from "../../../lib/stores/interfaces/accountStore.interface";
+import { sql } from "@vercel/postgres";
+
 const ProfilePage = async () => {
   const session = await auth();
 
   if (!session?.user) redirect(routes.account);
 
-  return <Profile user={session.user} />;
+  const user = await sql<IUser>`SELECT * FROM users WHERE email=${session?.user.email}`;
+
+  console.log(user.rows[0], "user object");
+  console.log(user.fields, "fields");
+
+  return <Profile user={user.rows[0]} fields={user.fields} />;
+
 };
 
 export default ProfilePage;
